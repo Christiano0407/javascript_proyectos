e9b9b88fb7588b79856ace8add72262c9550f071
Christiano0407 <placebo0704@gmail.com>
2020-02-03 09:20:28 -0600

    no-terminado

---
+++ javascript/hover.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+ parent: document.querySelector(`.distortion`),
+ intensity: 0.2,
+ image1: `./grayscale-photo-of-topless-woman-1164674.jpg`,
+ image2: `./topless-woman-1164829.jpg`,
+ displacementImage: `./stripe1.png`
+});
