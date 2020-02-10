19296d62f50c5142d1463f13cf91f0ee816a5dc3
Christiano0407 <placebo0704@gmail.com>
2020-02-10 12:05:20 -0600

    splash

--- javascript/texts.js
+++ javascript/texts.js
@@ -2,4 +2,26 @@
  const strText = text.textContent;
 
  const splitText = strText.split("");
- console.log(splitText);
+ text.textContent = "";
+
+ for(let i = 0; i < splitText.length; i++) {
+     text.innerHTML += "<span>" + splitText[i] + "</span>";
+ }
+
+ let char = 0;
+ let timer = setInterval(onTick,50);
+
+ function onTick() {
+     const span = text.querySelectorAll("span")[char];
+     span.classList.add("fade");
+     char++;
+     if (char === splitText.length) {
+         complete();
+         return;
+     }
+ }
+
+ function complete() {
+     clearInterval(timer);
+     timer = null;
+ }
