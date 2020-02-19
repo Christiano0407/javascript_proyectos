a8f527ce6d0a7aa822a8daa8de82932c57028685
Christiano0407 <placebo0704@gmail.com>
2020-02-19 12:18:45 -0600

    listas

--- javascript/nave.js
+++ javascript/nave.js
@@ -1,3 +1,16 @@
 const page = new fullpage(`#fullpage`, {
-    /* sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'], */
+    autoScrolling:true,
+    fitToSection:false,
+     easing:`easeInOutCubic`, 
+     scrollingSpeed:700, 
+     css3:true,
+     easingcss3:`ease-out`,
+    loopBottom:true,
+    /*  sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],  */
+    //navigation
+    navigation:true,
+    menu:`#menu`,
+    anchors:[`start`,`product`, `contact`],
+    navigationToolTips:[`start`,`product`, `contact`],
+    showActiveTooltip:false,
 });
